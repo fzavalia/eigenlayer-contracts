82df6c8b0363adbb2e75f0d532e0a53ec814947a
Yash Patil <40046473+ypatil12@users.noreply.github.com>
2025-01-24 15:54:37 -0500

    chore: remove logs

--- src/test/integration/IntegrationBase.t.sol
+++ src/test/integration/IntegrationBase.t.sol
@@ -794,9 +794,6 @@ abstract contract IntegrationBase is IntegrationDeployer {
         // Use timewarp to get previous staker shares
         int[] memory prevShares = _getPrevStakerDepositSharesInt(staker, strategies);
 
-        console.log("prevShares", prevShares[0]);
-        console.log("shareDeltas", shareDeltas[0]);
-        console.log("curShares", curShares[0]);
         // For each strategy, check (prev + added == cur)
         for (uint i = 0; i < strategies.length; i++) {
             assertEq(prevShares[i] + shareDeltas[i], curShares[i], err);
